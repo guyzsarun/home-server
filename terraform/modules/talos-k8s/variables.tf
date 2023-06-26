ffb9b6a7d0f270ce0aeddccc8be9d0ae2252d20c
guyzsarun <sarun.nunt@gmail.com>
2023-06-26 21:59:40 +0200

    talos module

---
+++ terraform/modules/talos-k8s/variables.tf
@@ -0,0 +1,19 @@
+variable "proxmox_config" {
+  type      = map(any)
+  sensitive = true
+  default = {
+    api_url  = "https://proxmox-server01.example.com:8006/api2/json"
+    ip       = "192.168.1.1"
+    user     = "root@localhost"
+    ssh_user = "root"
+    password = "password"
+  }
+}
+
+variable "talos" {
+  type = map(string)
+  default = {
+    "version"      = "v1.4.4"
+    "worker_count" = 2
+  }
+}
