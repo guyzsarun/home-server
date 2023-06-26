c16a9f4b7729e3c8c12b5809af6af98586047836
guyzsarun <sarun.nunt@gmail.com>
2023-06-26 21:54:15 +0200

    vm format

---
+++ terraform/modules/vm/variables.tf
@@ -0,0 +1,19 @@
+variable "proxmox_config" {
+  type      = map(any)
+  sensitive = true
+  default = {
+    api_url  = "https://proxmox-server01.example.com:8006/api2/json"
+    ip       = "192.168.1.1"
+    user     = "root@localhost"
+    ssh_user = "root"
+    password = "password"
+  }
+}
+
+variable "pfsense" {
+  type = map(string)
+  default = {
+    "version"  = "2.5.2"
+    "checksum" = "941a68c7f20c4b635447cceda429a027f816bdb78d54b8252bb87abf1fc22ee3"
+  }
+}
