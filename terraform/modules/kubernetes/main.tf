9b87cf94d6987974cb1b5e4398780607512a5f97
guyzsarun <sarun.nunt@gmail.com>
2023-10-09 20:23:21 +0200

    add kong terraform

--- terraform/modules/kubernetes/main.tf
+++ terraform/modules/kubernetes/main.tf
@@ -48,6 +48,35 @@ resource "kubernetes_namespace" "istio-system" {
   }
 }
 
+resource "kubernetes_namespace" "kong-gateway" {
+  metadata {
+    name = "kong-gateway"
+    labels = {
+        "istio-injection" = "enabled"
+        "pod-security.kubernetes.io/enforce" = "privileged"
+        "pod-security.kubernetes.io/audit"   = "privileged"
+        "pod-security.kubernetes.io/warn"    = "privileged"
+    }
+  }
+}
+
+resource "helm_release" "kong-gateway" {
+  name       = "kong-gateway"
+  repository = "https://charts.konghq.com"
+  chart      = "kong"
+  version    = "2.29.0"
+
+  namespace = "kong-gateway"
+  values = [
+    "${file("../kubernetes/kong/kong-values.yaml")}"
+  ]
+
+  depends_on = [
+    kubernetes_namespace.kong-gateway
+    ]
+}
+
+
 resource "helm_release" "kube-prom-stack" {
   name       = "monitoring"
   repository = "https://prometheus-community.github.io/helm-charts"
