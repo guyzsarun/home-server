0040ab5280c389fc720537a0409508abc99747fe
Sarun Nuntaviriyakul <48861591+guyzsarun@users.noreply.github.com>
2023-10-27 22:41:50 +0200

    Feature: ELK stack (#16)
    * Move prom stack to addons
    
    * Add k8s addons
    
    * Add elastic / kibana
    
    * add kibana vs
    
    * add fluentbit
    
    * fluent-bit k8s addons

--- terraform/modules/kubernetes-addons/main.tf
+++ terraform/modules/kubernetes-addons/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
+  }
+}
+
 resource "kubernetes_namespace" "kong-gateway" {
   metadata {
     name = "kong-gateway"
@@ -16,12 +25,63 @@ resource "helm_release" "kong-gateway" {
   chart      = "kong"
   version    = "2.29.0"
 
-  namespace = "kong-gateway"
+  namespace = kubernetes_namespace.kong-gateway.metadata[0].name
   values = [
     "${file("../kubernetes/kong/kong-values.yaml")}"
   ]
+}
 
-  depends_on = [
-    kubernetes_namespace.kong-gateway
+resource "helm_release" "elastic-operator" {
+  name       = "elastic-operator"
+  repository = "https://helm.elastic.co"
+  chart      = "eck-operator"
+  version    = "2.9.0"
+
+  namespace = kubernetes_namespace.istio-system.metadata[0].name
+}
+
+data "kubectl_path_documents" "elk" {
+    pattern = "../kubernetes/elk/*.yaml"
+}
+
+resource "kubectl_manifest" "elk-kibana" {
+    for_each  = toset(data.kubectl_path_documents.elk.documents)
+    yaml_body = each.value
+    override_namespace = kubernetes_namespace.istio-system.metadata[0].name
+}
+
+resource "helm_release" "fluent-bit" {
+  name       = "fluent-bit"
+  repository = "https://fluent.github.io/helm-charts"
+  chart      = "fluent-bit"
+  version    = "0.39.0"
+
+  namespace = "kube-system"
+  values = [
+    "${file("../kubernetes/elk/fluent-bit-values.yml")}"
   ]
+}
+
+resource "helm_release" "kube-prom-stack" {
+  name       = "monitoring"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  version    = "48.3.1"
+
+  namespace = kubernetes_namespace.istio-system.metadata[0].name
+  values = [
+    "${file("../kubernetes/monitoring/kube-prom-values.yaml")}"
+  ]
+
+}
+
+resource "kubernetes_namespace" "istio-system" {
+  metadata {
+    name = "istio-system"
+    labels = {
+      "pod-security.kubernetes.io/enforce" = "privileged"
+      "pod-security.kubernetes.io/audit"   = "privileged"
+      "pod-security.kubernetes.io/warn"    = "privileged"
+    }
+  }
 }
